765a4d2eeaa41735e7f1de9c79fbaf8380fddd60
Matthias <itgkarl@web.de>
2012-12-05 15:17:28 +0100

    bill

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121205113634) do
+ActiveRecord::Schema.define(:version => 20121205141539) do
 
   create_table "bills", :force => true do |t|
     t.float    "value"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20121205113634) do
     t.datetime "updated_at",    :null => false
     t.integer  "user_id"
     t.integer  "coffee_box_id"
+    t.boolean  "flagDisabled"
   end
 
   create_table "expenses", :force => true do |t|
