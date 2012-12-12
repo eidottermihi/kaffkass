5c65adc6f9cd85bb76778d24cbbc3efb4498d22e
eidottermihi <eidottermihi@googlemail.com>
2012-12-12 15:34:37 +0100

    schema.rb auf konsistenten Status gebracht.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121205145049) do
+ActiveRecord::Schema.define(:version => 20121212124326) do
 
   create_table "bills", :force => true do |t|
     t.float    "value"
