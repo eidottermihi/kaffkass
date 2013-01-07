f97bb7266ff4b7523cf937d2c46f0efa67bc758d
itgkarl <p76ciqka>
2013-01-07 23:22:26 +0100

    mailing überarbeitet

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,20 +3,23 @@ class Notifier < ActionMailer::Base
 
   def activation_instructions(user)
     @user = user
-    mail(to: user.email, from: 'no-reply@kaffkass.com',
+    mail = @user.email
+    mail(to: mail, from: 'no-reply@kaffkass.com',
          subject: "KaffKass Accountaktivierung")
   end
 
   def welcome(user)
     @user = user
-    mail(to: user.email, from: 'no-reply@kaffkass.com',
+    mail = @user.email
+    mail(to: mail, from: 'no-reply@kaffkass.com',
          subject: "Willkommen bei KaffKass!")
   end
 
   def bill(bill, user)
     @user = user
+    mail = @user.email
     @bill = bill
-    mail(to: user.email, from: 'no-reply@kaffkass.com',
+    mail(to: mail, from: 'no-reply@kaffkass.com',
          subject: "KaffKass - Rechnung für #{bill.date}")
   end
 
